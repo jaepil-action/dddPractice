2159fb5f69256fcc2549e1cccdee98f5b944465e
jaepil <76797962+jps091@users.noreply.github.com>
2024-10-26 09:51:02 +0900

    Update Ddd2Application.java

--- src/main/java/myproject/DDD2/Ddd2Application.java
+++ src/main/java/myproject/DDD2/Ddd2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ddd2Application {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(Ddd2Application.class, args);
 	}
